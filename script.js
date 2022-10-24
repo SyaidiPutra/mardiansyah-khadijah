2bd7fa56192d269b15c745dd197e02edcc86bc43
SyaidiPutra <syaidiputra29@gmail.com>
2022-10-24 23:24:22 +0800

    uplaod
    Co-authored-by: Reina Desvia <Sn0wyOwl@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
+AOS.init();
+
 const myModal = new bootstrap.Modal('#heroModel', {
     keyboard: false
 })
 myModal.show()
 
+var audio = document.querySelector('#mosek')
+var is_play = true
+
+function mosek(){
+    audio.play();
+    is_play = true
+    document.querySelector('#control_music').innerHTML=`<i class="fas fa-pause"></i>`
+}
+
+function sandak(){
+    const control = document.querySelector('#control_music');
+    const status = document.querySelector('#music_status');
+    if(is_play == true){
+        audio.pause();
+        is_play = false
+        control.innerHTML=`<i class="fas fa-play"></i>`
+        status.innerHTML = 'Pause'
+    }else{
+        audio.play();
+        is_play = true
+        control.innerHTML=`<i class="fas fa-pause"></i>`
+        status.innerHTML = 'Playing...'
+    }
+}
